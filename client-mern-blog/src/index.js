5dc8aba03464e0c5ad2a00f3dc06121e3fbc9aa3
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-26 19:12:05 +0500

    add gloabal styles

--- client-mern-blog/src/index.js
+++ client-mern-blog/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './index.css'
 import { store } from './redux/store'
+import './styles/global.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(

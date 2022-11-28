5a0ceeed7763928f8acb4d13a9d501379bbadf1f
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-28 17:43:33 +0500

    redesing login/register page

--- client-mern-blog/src/pages/RegisterPage/RegisterPage.jsx
+++ client-mern-blog/src/pages/RegisterPage/RegisterPage.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { checkIsAuth, registerUser } from '../../redux/features/auth/authSlice'
+import s from './RegisterPage.module.scss'
 
 export const RegisterPage = () => {
   const [username, setUsername] = useState('')
@@ -35,48 +36,50 @@ export const RegisterPage = () => {
   }
 
   return (
-    <form
-      onSubmit={(e) => e.preventDefault()}
-      className="w-1/4 h-60 mx-auto mt-40"
-    >
-      <h1 className="text-lg text-white text-center">Регистрация</h1>
-      <label className="text-xs text-gray-400">
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Username"
-          className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-700"
-        />
-      </label>
+    <div className="container">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className={s.inner}
+      >
+        <h1 className="text-lg text-white text-center">Регистрация</h1>
+        <label className={s.forms}>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Логин..."
+            className={s.inputs}
+          />
+        </label>
 
-      <label className="text-xs text-gray-400">
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-700"
-        />
-      </label>
+        <label className={s.forms}>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Пароль..."
+            className={s.inputs}
+          />
+        </label>
 
-      <div className="flex gap-8 justify-center mt-4">
-        <button
-          type="submit"
-          onClick={handleSubmit}
-          className="flex justify-center items-center text-xs bg-gray-600 text-white rounded-sm py-2 px-4"
-        >
-          Подтвердить
-        </button>
-        <Link
-          to="/login"
-          className="flex justify-center items-center text-xs text-white"
-        >
-          Уже зарегистрированы ?
-        </Link>
-      </div>
-    </form>
+        <div className="flex gap-8 justify-center mt-4">
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            className={s.form__btn}
+          >
+            Подтвердить
+          </button>
+          <Link
+            to="/login"
+            className="flex justify-center items-center text-xs text-white"
+          >
+            Уже зарегистрированы ?
+          </Link>
+        </div>
+      </form>
+    </div>
   )
 }

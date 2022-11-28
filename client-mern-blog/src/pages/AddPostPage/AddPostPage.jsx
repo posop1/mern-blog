e8ed207b7d86649596e313c8eb908414d9b005b0
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-28 17:28:49 +0500

    redesing addPostPage

--- client-mern-blog/src/pages/AddPostPage/AddPostPage.jsx
+++ client-mern-blog/src/pages/AddPostPage/AddPostPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { createPost } from '../../redux/features/post/postSlice'
+import s from './AddPostPage.module.scss'
 
 export const AddPostPage = () => {
   const [title, setTitle] = useState('')
@@ -29,63 +30,48 @@ export const AddPostPage = () => {
   }
 
   return (
-    <form
-      className="w-1/3 mx-auto py-10"
-      onSubmit={(e) => e.preventDefault()}
-    >
-      <label className="text-gray-300 py-2 bg-gray-600 text-xs mt-2 flex items-center justify-center border-2 border-dotted cursor-pointer">
-        Прикрепить изорбажение:
-        <input
-          type="file"
-          className="hidden"
-          onChange={(e) => setImage(e.target.files[0])}
-        />
-      </label>
-      <div className="flex object-cover py-2">
-        {image && (
-          <img
-            src={URL.createObjectURL(image)}
-            alt={image.name}
+    <div className="container">
+      <form
+        className={s.AddPostPage}
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <label>
+          Заголовок поста:
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Заголовок..."
+            className={s.input}
           />
-        )}
-      </div>
+        </label>
 
-      <label className="text-xs text-white opacity-70">
-        Заголовок поста:
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="Заголовок"
-          className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-700"
-        />
-      </label>
+        <label>
+          Текст поста:
+          <textarea
+            onChange={(e) => setText(e.target.value)}
+            value={text}
+            placeholder="Текст поста..."
+            className={s.input}
+          />
+        </label>
 
-      <label className="text-xs text-white opacity-70">
-        Текст поста:
-        <textarea
-          onChange={(e) => setText(e.target.value)}
-          value={text}
-          placeholder="Текст поста"
-          className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none resize-none h-40 placeholder:text-gray-700"
-        />
-      </label>
+        <div>
+          <button
+            onClick={submitHandler}
+            className={s.btns}
+          >
+            Добавить
+          </button>
 
-      <div className="flex gap-8 items-center justify-center mt-4">
-        <button
-          onClick={submitHandler}
-          className="flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm py-2 px-4"
-        >
-          Добавить
-        </button>
-
-        <button
-          onClick={clearFormHandler}
-          className="flex justify-center items-center bg-red-500 text-xs text-white rounded-sm py-2 px-4"
-        >
-          Отменить
-        </button>
-      </div>
-    </form>
+          <button
+            onClick={clearFormHandler}
+            className={s.btns}
+          >
+            Отменить
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
